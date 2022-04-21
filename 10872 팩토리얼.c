fe96211d9f2d16a1409ffc1071bdd138a1d7ab7c
DDingshark <103184052+DDingshark@users.noreply.github.com>
2022-04-21 21:24:17 +0900

    Add files via upload

---
+++ 10872 팩토리얼.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+
+	if (A > B)
+	{
+		printf("'>'");
+	}
+	else if (A == B)
+	{
+		printf("\=\=");
+	}
+	else if (A < B)
+	{
+		printf("'<'");
+	}
+
+
+	return 0;
+}
