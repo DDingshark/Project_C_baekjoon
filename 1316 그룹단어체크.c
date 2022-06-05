33be81a5a291c23b6d288c012ad26c153f89e47a
DDING <103184052+DDingshark@users.noreply.github.com>
2022-06-05 18:36:59 +0900

    Add files via upload

---
+++ 1316 그룹단어체크.c
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//떨어져서 나오면 안된다... 
+//이전거 저장 , 다음것이 나왔는대 0번째나온
+//것이 아닌대 이전거랑 다르다 
+int main()
+{
+	int n,count,word_g=0;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int alpa[28] = { 0, };
+		count = 0;
+		char str[101];
+		scanf_s("%s", str, sizeof(str));
+		int len = strlen(str);
+		alpa[27] = -1;
+		for (int j = 0; j < len; j++)
+		{
+			int k = (int)str[j] - 97;
+			if (alpa[k] != 0 && alpa[27] != k)
+			{
+				count += 1;
+			}
+			alpa[k] += 1;
+			alpa[27] = k;
+		}
+		if (count == 0)
+		{
+			word_g += 1;
+		}
+	}
+	printf("%d", word_g);
+	return 0;
+}
---
+++ 9012 괄호.c
@@ -0,0 +1,51 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//(의 수랑 )의 수가 같으면 가능?
+//이해 완료 (의수보다 )수가 작으면 x 
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int count_l = 0, count_r = 0, debug = 0;
+		char str[51];
+		scanf_s("%s", str,sizeof(str));
+		int len = strlen(str);
+		for (int i = 0; i < len; i++)
+		{
+			if (str[i] == '(')
+			{
+				count_l += 1;
+			}
+			else if(str[i] == ')')
+			{
+				count_r += 1;
+			}
+			if (count_r > count_l)
+			{
+				printf("NO\n");
+				debug = 1;
+				break;
+			}
+		}
+
+		if (count_l == count_r)
+		{
+			printf("YES\n");
+		}
+		else if(debug ==1)
+		{
+			continue;
+		}
+		else
+		{
+			printf("NO\n");
+		}
+	}
+
+
+	return 0;
+}
